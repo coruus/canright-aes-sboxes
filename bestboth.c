a3f70841361ea063ff2621af28f29026d9f1b38f
David Leon Gil <coruus@gmail.com>
2014-08-10 13:30:29 -0400

    clang-format

--- bestboth.c
+++ bestboth.c
@@ -1,4 +1,4 @@
-/* bestboth.c   
+/* bestboth.c
  *
  * by: David Canright
  *
@@ -17,13 +17,13 @@ basis #  4:
  ncols =  8, gates = 42
   A2Xb: 0000000000012804100810224008808001
   S2Xb: 0028006200000100008800000102044010
- [0,2],  [0,3],  [1,7],  [2,10],  [3,11],  [4,7],  [5,8],  [6,10],  [4,15], 
+ [0,2],  [0,3],  [1,7],  [2,10],  [3,11],  [4,7],  [5,8],  [6,10],  [4,15],
  ncols = 17, gates = 20
    X2S: 582D9E0BDC040324   X2A: 64786E8C6829DE60
  ncols =  8, gates = 38
   X2Sb: 000000000000000040082480180002040100
   X2Ab: 041000800021D00000000000000204080860
- [0,4],  [1,3],  [1,7],  [2,4],  [2,8],  [2,6],  [3,13],  [5,11],  [6,9],  [10,12], 
+ [0,4],  [1,3],  [1,7],  [2,4],  [2,8],  [2,6],  [3,13],  [5,11],  [6,9],  [10,12],
  ncols = 18, gates = 18
 ***bestgates   4 =    38   =   20 +   18
  * which, for each matrix pair, shows the original versions (8 columns),
@@ -41,167 +41,176 @@ F1261450CA86D330C502A8BF412B3590352582D03974323C65C4836C69953380    0
 
 /* gatematrix is a structure with an array of 16-bit columns,
  list of indices (used in pairs), number of columns, and number of gates*/
-typedef struct gatematrix
-     { unsigned int mat[128]; char ind[256]; int n; int g; }
-     GateMat;
+typedef struct gatematrix {
+  unsigned int mat[128];
+  char ind[256];
+  int n;
+  int g;
+} GateMat;
 
 static unsigned int share[65536];
 static GateMat test;
 
 /* blockPrint prints columns and index pairs for matrix pair */
-void blockPrint (GateMat *p, const char *tag1, const char *tag2)
-{
-     int i;
+void blockPrint(GateMat* p, const char* tag1, const char* tag2) {
+  int i;
 
-     printf ("%6s: ", tag1);
-     for (i = 0; i < p->n; i++) 
-          printf ("%02X", (p->mat[i]) & 0XFF );
-     if ((p->n) > N) printf ("\n");
-     printf ("%6s: ", tag2);
-     for (i = 0; i < p->n; i++) 
-          printf ("%02X", ((p->mat[i]) & 0XFF00) >> 8 );
-     if ((p->n) > N) printf ("\n");
-     for (i = 0; i < (p->n)-N; i++) 
-          printf (" [%1d,%1d], ", p->ind[2*i], p->ind[2*i+1]);
-     printf ("\n ncols = %2d, gates = %2d\n", p->n, p->g);
-          
+  printf("%6s: ", tag1);
+  for (i = 0; i < p->n; i++) printf("%02X", (p->mat[i]) & 0XFF);
+  if ((p->n) > N)
+    printf("\n");
+  printf("%6s: ", tag2);
+  for (i = 0; i < p->n; i++) printf("%02X", ((p->mat[i]) & 0XFF00) >> 8);
+  if ((p->n) > N)
+    printf("\n");
+  for (i = 0; i < (p->n) - N; i++)
+    printf(" [%1d,%1d], ", p->ind[2 * i], p->ind[2 * i + 1]);
+  printf("\n ncols = %2d, gates = %2d\n", p->n, p->g);
+
 } /* end blockPrint */
 
 /* copyMat copies from one to another*/
-void copyMat (GateMat *p, GateMat *q)
-{
-     int i, n;
-     
-     n = q->n = p->n;
-     q->g = p->g;
-     memcpy( q->mat, p->mat, n * sizeof(unsigned int));
-     memcpy( q->ind, p->ind, (n - N)*2);
+void copyMat(GateMat* p, GateMat* q) {
+  int i, n;
+
+  n = q->n = p->n;
+  q->g = p->g;
+  memcpy(q->mat, p->mat, n * sizeof(unsigned int));
+  memcpy(q->ind, p->ind, (n - N) * 2);
 } /* end copyMat */
 
-/* 
+/*
 * bestgates is recursive:
 *   takes current matrix, tries all possibilities of adding a gate
 *   returns best # of gates
 * p points to test matrix on input, and used to store output.
 * tree search is pruned if this set of columns previously tried
 */
-void bestgates ()
-{
-     char indb[256];
-     int gb, nb, ci, cj;
-     int i, j, n, c, g, io, jo;
-     int nm, np, n2, n2p, t;
-     
-     gb = 1024;  /* best # gates, start high */
-     n = test.n; g = test.g; 
-     nm=n-1; np=n+1; n2=2*(n-N); n2p=n2+1;
-     if (n==N) io = jo = 0;  /* if orig matrix, no "old" index pair */
-     else { io = (test.ind[n2-2]); jo = (test.ind[n2-1]); }
-     for (i=0;i<nm;i++)   /* for each pair of columns */
-     for (j=i+1;j<n;j++) {
-     c = (test.mat[i]) & (test.mat[j]);
-     if (t=share[c]) {   /* if can share a gate */
-          if (i<io && j!=io && j!=jo && j<nm)  /* if prior, indep. pair */
-               continue; /* then been there, done that; skip to next j */
-          test.n = np;
-          test.g = g - t;
-          ci = test.mat[i];  /* save current columns */
-          cj = test.mat[j];
-          test.mat[i] ^= c;  /* update to new columns */
-          test.mat[j] ^= c;
-          test.mat[n] = c;
-          test.ind[n2] = i;
-          test.ind[n2p] = j;
-          bestgates();       /* recurse with new matrix */
-          test.mat[i] = ci;  /* restore current columns */
-          test.mat[j] = cj;
-          if ( test.g < gb ) {  /* if best yet, save data */
-               memcpy( indb, test.ind+n2, (test.n - n)*2);
-               nb = test.n;
-               gb = test.g;
-               }
-          }
-     }   /* end columns loop */
-     if (gb < 1024) {   /* if improved, return best data */
-          memcpy( test.ind+n2, indb, (nb - n)*2);
-          test.n = nb;
-          test.g = gb;
-          }
-/*      else {printf("%3d [%2d]",n,g); fflush(stdout);} */
+void bestgates() {
+  char indb[256];
+  int gb, nb, ci, cj;
+  int i, j, n, c, g, io, jo;
+  int nm, np, n2, n2p, t;
+
+  gb = 1024; /* best # gates, start high */
+  n = test.n;
+  g = test.g;
+  nm = n - 1;
+  np = n + 1;
+  n2 = 2 * (n - N);
+  n2p = n2 + 1;
+  if (n == N)
+    io = jo = 0; /* if orig matrix, no "old" index pair */
+  else {
+    io = (test.ind[n2 - 2]);
+    jo = (test.ind[n2 - 1]);
+  }
+  for (i = 0; i < nm; i++) /* for each pair of columns */
+    for (j = i + 1; j < n; j++) {
+      c = (test.mat[i]) & (test.mat[j]);
+      if (t = share[c]) {                           /* if can share a gate */
+        if (i < io && j != io && j != jo && j < nm) /* if prior, indep. pair */
+          continue; /* then been there, done that; skip to next j */
+        test.n = np;
+        test.g = g - t;
+        ci = test.mat[i]; /* save current columns */
+        cj = test.mat[j];
+        test.mat[i] ^= c; /* update to new columns */
+        test.mat[j] ^= c;
+        test.mat[n] = c;
+        test.ind[n2] = i;
+        test.ind[n2p] = j;
+        bestgates();      /* recurse with new matrix */
+        test.mat[i] = ci; /* restore current columns */
+        test.mat[j] = cj;
+        if (test.g < gb) { /* if best yet, save data */
+          memcpy(indb, test.ind + n2, (test.n - n) * 2);
+          nb = test.n;
+          gb = test.g;
+        }
+      }
+    }              /* end columns loop */
+  if (gb < 1024) { /* if improved, return best data */
+    memcpy(test.ind + n2, indb, (nb - n) * 2);
+    test.n = nb;
+    test.g = gb;
+  }
+  /*      else {printf("%3d [%2d]",n,g); fflush(stdout);} */
 } /* end bestgates */
 
 /* bestmat reconstructs best matrix */
-void bestmat (GateMat *p)
-{
-     int i, j, n, c;
-     int nm, np, n2, n2p, t;
-     GateMat best;
-     
-     p->g = test.g; 
-     for (i=0;i<N;i++) test.mat[i] = p->mat[i];
-     for (n=0;n<(test.n-N);n++) {
-     i = test.ind[n*2];
-     j = test.ind[n*2+1];
-     c = (test.mat[i]) & (test.mat[j]);
-     test.mat[i] ^= c;
-     test.mat[j] ^= c;
-     test.mat[n+N] = c;
-     }
+void bestmat(GateMat* p) {
+  int i, j, n, c;
+  int nm, np, n2, n2p, t;
+  GateMat best;
+
+  p->g = test.g;
+  for (i = 0; i < N; i++) test.mat[i] = p->mat[i];
+  for (n = 0; n < (test.n - N); n++) {
+    i = test.ind[n * 2];
+    j = test.ind[n * 2 + 1];
+    c = (test.mat[i]) & (test.mat[j]);
+    test.mat[i] ^= c;
+    test.mat[j] ^= c;
+    test.mat[n + N] = c;
+  }
 } /* end bestmat */
 
 /* main */
-int main( int argc, char *argv[] ){
-     char   line[256];
-     char   name[4][4] = {"A2X", "X2S", "S2X", "X2A", };
-     char   bname[4][5] = {"A2Xb", "X2Sb", "S2Xb", "X2Ab", };
-     long int i, j, k, n, nid, gt;
-     unsigned u;
-     int InitMat[32];
-     GateMat orig[2];
+int main(int argc, char* argv[]) {
+  char line[256];
+  char name[4][4] = {
+      "A2X", "X2S", "S2X", "X2A",
+  };
+  char bname[4][5] = {
+      "A2Xb", "X2Sb", "S2Xb", "X2Ab",
+  };
+  long int i, j, k, n, nid, gt;
+  unsigned u;
+  int InitMat[32];
+  GateMat orig[2];
 
-/* share[i] is initialized to 0 if # bits < 2 */
-share[0] = 0;
-for (i=1;i<65536;i++) { 
-     k=0;
-     for (j=i&0xFFFF; j; j >>=1) k += j&1;
-     share[i] = k-1;
-     }
+  /* share[i] is initialized to 0 if # bits < 2 */
+  share[0] = 0;
+  for (i = 1; i < 65536; i++) {
+    k = 0;
+    for (j = i & 0xFFFF; j; j >>= 1) k += j & 1;
+    share[i] = k - 1;
+  }
 
-while ( fgets( line, 256, stdin ) == line ) {
-     for ( i=0; i < 32; i++ ) {   /* read matrices, ID number */
-          sscanf( line+2*i, "%02X", &u );
-          InitMat[i] = u;
-          } 
-          sscanf( line+65, "%d", &nid );
-     printf("\nbasis #%3d:\n", nid);
+  while (fgets(line, 256, stdin) == line) {
+    for (i = 0; i < 32; i++) { /* read matrices, ID number */
+      sscanf(line + 2 * i, "%02X", &u);
+      InitMat[i] = u;
+    }
+    sscanf(line + 65, "%d", &nid);
+    printf("\nbasis #%3d:\n", nid);
 
-/* NOTE: matrix input order is: [A2X, X2A, X2S, S2X] */
-     for (i=0;i<8;i++) {    /* combine input pair; combine output pair */
-          (orig[0]).mat[i] = InitMat[8*0+i] | (InitMat[8*3+i] <<8) ;
-          (orig[1]).mat[i] = InitMat[8*2+i] | (InitMat[8*1+i] <<8) ;
-          }
-     
-     gt = 0;
-     for (k=0;k<2;k++) {   /* for each matrix pair */
-     (orig[k]).n = 8;   /* initialize # columns, # gates */
-     for (i=j=0; i<8; i++) j += share[ (orig[k]).mat[i] ];
-     (orig[k]).g = j - 8;
-     blockPrint (&(orig[k]), name[k], name[k+2]);
-     fflush(stdout);
- 
-     copyMat(&(orig[k]), &test);
-     bestgates();   /* optimize */
-     bestmat(&(orig[k]));
- 
-     blockPrint (&test, bname[k], bname[k+2]);
-     fflush(stdout);
-     gt += test.g;   /* total # gates */
+    /* NOTE: matrix input order is: [A2X, X2A, X2S, S2X] */
+    for (i = 0; i < 8; i++) { /* combine input pair; combine output pair */
+      (orig[0]).mat[i] = InitMat[8 * 0 + i] | (InitMat[8 * 3 + i] << 8);
+      (orig[1]).mat[i] = InitMat[8 * 2 + i] | (InitMat[8 * 1 + i] << 8);
+    }
 
-     }
-     printf("***bestgates %3d = %5d   =%5d +%5d\n",
-       nid, gt, (orig[0]).g, (orig[1]).g );
-     fflush(stdout);
-     }
+    gt = 0;
+    for (k = 0; k < 2; k++) { /* for each matrix pair */
+      (orig[k]).n = 8;        /* initialize # columns, # gates */
+      for (i = j = 0; i < 8; i++) j += share[(orig[k]).mat[i]];
+      (orig[k]).g = j - 8;
+      blockPrint(&(orig[k]), name[k], name[k + 2]);
+      fflush(stdout);
 
-    return(0);
-    } /* end main */
+      copyMat(&(orig[k]), &test);
+      bestgates(); /* optimize */
+      bestmat(&(orig[k]));
+
+      blockPrint(&test, bname[k], bname[k + 2]);
+      fflush(stdout);
+      gt += test.g; /* total # gates */
+    }
+    printf("***bestgates %3d = %5d   =%5d +%5d\n", nid, gt, (orig[0]).g, (orig[1]).g);
+    fflush(stdout);
+  }
+
+  return (0);
+} /* end main */
--- sboxalg.c
+++ sboxalg.c
@@ -1,4 +1,4 @@
-/* sbox.c   
+/* sbox.c
  *
  * by: David Canright
  *
@@ -12,158 +12,168 @@
 
 /* to convert between polynomial (A^7...1) basis A & normal basis X */
 /* or to basis S which incorporates bit matrix of Sbox */
-static int 
-    A2X[8] = {0x98, 0xF3, 0xF2, 0x48, 0x09, 0x81, 0xA9, 0xFF},
-    X2A[8] = {0x64, 0x78, 0x6E, 0x8C, 0x68, 0x29, 0xDE, 0x60},
-    X2S[8] = {0x58, 0x2D, 0x9E, 0x0B, 0xDC, 0x04, 0x03, 0x24},
-    S2X[8] = {0x8C, 0x79, 0x05, 0xEB, 0x12, 0x04, 0x51, 0x53};
+static int A2X[8] = {0x98, 0xF3, 0xF2, 0x48, 0x09, 0x81, 0xA9, 0xFF},
+           X2A[8] = {0x64, 0x78, 0x6E, 0x8C, 0x68, 0x29, 0xDE, 0x60},
+           X2S[8] = {0x58, 0x2D, 0x9E, 0x0B, 0xDC, 0x04, 0x03, 0x24},
+           S2X[8] = {0x8C, 0x79, 0x05, 0xEB, 0x12, 0x04, 0x51, 0x53};
 
 /* multiply in GF(2^2), using normal basis (Omega^2,Omega) */
-int G4_mul( int x, int y ) {
-    int a, b, c, d, e, p, q;
+int G4_mul(int x, int y) {
+  int a, b, c, d, e, p, q;
 
-    a = (x & 0x2) >> 1; b = (x & 0x1);
-    c = (y & 0x2) >> 1; d = (y & 0x1);
-    e = (a ^ b) & (c ^ d);
-    p = (a & c) ^ e;
-    q = (b & d) ^ e;
-    return ( (p<<1) | q );
-    }
+  a = (x & 0x2) >> 1;
+  b = (x & 0x1);
+  c = (y & 0x2) >> 1;
+  d = (y & 0x1);
+  e = (a ^ b) & (c ^ d);
+  p = (a & c) ^ e;
+  q = (b & d) ^ e;
+  return ((p << 1) | q);
+}
 
 /* scale by N = Omega^2 in GF(2^2), using normal basis (Omega^2,Omega) */
-int G4_scl_N( int x ) {
-    int a, b, p, q;
+int G4_scl_N(int x) {
+  int a, b, p, q;
 
-    a = (x & 0x2) >> 1; b = (x & 0x1);
-    p = b;
-    q = a ^ b;
-    return ( (p<<1) | q );
-    }
+  a = (x & 0x2) >> 1;
+  b = (x & 0x1);
+  p = b;
+  q = a ^ b;
+  return ((p << 1) | q);
+}
 
 /* scale by N^2 = Omega in GF(2^2), using normal basis (Omega^2,Omega) */
-int G4_scl_N2( int x ) {
-    int a, b, p, q;
+int G4_scl_N2(int x) {
+  int a, b, p, q;
 
-    a = (x & 0x2) >> 1; b = (x & 0x1);
-    p = a ^ b;
-    q = a;
-    return ( (p<<1) | q );
-    }
+  a = (x & 0x2) >> 1;
+  b = (x & 0x1);
+  p = a ^ b;
+  q = a;
+  return ((p << 1) | q);
+}
 
 /* square in GF(2^2), using normal basis (Omega^2,Omega) */
 /* NOTE: inverse is identical */
-int G4_sq( int x ) {
-    int a, b;
+int G4_sq(int x) {
+  int a, b;
 
-    a = (x & 0x2) >> 1; b = (x & 0x1);
-    return ( (b<<1) | a );
-    }
+  a = (x & 0x2) >> 1;
+  b = (x & 0x1);
+  return ((b << 1) | a);
+}
 
 /* multiply in GF(2^4), using normal basis (alpha^8,alpha^2) */
-int G16_mul( int x, int y ) {
-    int a, b, c, d, e, p, q;
+int G16_mul(int x, int y) {
+  int a, b, c, d, e, p, q;
 
-    a = (x & 0xC) >> 2; b = (x & 0x3);
-    c = (y & 0xC) >> 2; d = (y & 0x3);
-    e = G4_mul( a ^ b, c ^ d );
-    e = G4_scl_N(e);
-    p = G4_mul( a, c ) ^ e;
-    q = G4_mul( b, d ) ^ e;
-    return ( (p<<2) | q );
-    }
+  a = (x & 0xC) >> 2;
+  b = (x & 0x3);
+  c = (y & 0xC) >> 2;
+  d = (y & 0x3);
+  e = G4_mul(a ^ b, c ^ d);
+  e = G4_scl_N(e);
+  p = G4_mul(a, c) ^ e;
+  q = G4_mul(b, d) ^ e;
+  return ((p << 2) | q);
+}
 
 /* square & scale by nu in GF(2^4)/GF(2^2), normal basis (alpha^8,alpha^2) */
 /*   nu = beta^8 = N^2*alpha^2, N = w^2 */
-int G16_sq_scl( int x ) {
-    int a, b, p, q;
+int G16_sq_scl(int x) {
+  int a, b, p, q;
 
-    a = (x & 0xC) >> 2; b = (x & 0x3);
-    p = G4_sq(a ^ b); 
-    q = G4_scl_N2(G4_sq(b)); 
-    return ( (p<<2) | q );
-    }
+  a = (x & 0xC) >> 2;
+  b = (x & 0x3);
+  p = G4_sq(a ^ b);
+  q = G4_scl_N2(G4_sq(b));
+  return ((p << 2) | q);
+}
 
 /* inverse in GF(2^4), using normal basis (alpha^8,alpha^2) */
-int G16_inv( int x ) {
-    int a, b, c, d, e, p, q;
+int G16_inv(int x) {
+  int a, b, c, d, e, p, q;
 
-    a = (x & 0xC) >> 2; b = (x & 0x3);
-    c = G4_scl_N( G4_sq( a ^ b ) );
-    d = G4_mul( a, b );
-    e = G4_sq( c ^ d );   // really inverse, but same as square
-    p = G4_mul( e, b );
-    q = G4_mul( e, a );
-    return ( (p<<2) | q );
-    }
+  a = (x & 0xC) >> 2;
+  b = (x & 0x3);
+  c = G4_scl_N(G4_sq(a ^ b));
+  d = G4_mul(a, b);
+  e = G4_sq(c ^ d);  // really inverse, but same as square
+  p = G4_mul(e, b);
+  q = G4_mul(e, a);
+  return ((p << 2) | q);
+}
 
 /* inverse in GF(2^8), using normal basis (d^16,d) */
-int G256_inv( int x ) {
-    int a, b, c, d, e, p, q;
+int G256_inv(int x) {
+  int a, b, c, d, e, p, q;
 
-    a = (x & 0xF0) >> 4; b = (x & 0x0F);
-    c = G16_sq_scl( a ^ b );
-    d = G16_mul( a, b );
-    e = G16_inv( c ^ d );
-    p = G16_mul( e, b );
-    q = G16_mul( e, a );
-    return ( (p<<4) | q );
-    }
+  a = (x & 0xF0) >> 4;
+  b = (x & 0x0F);
+  c = G16_sq_scl(a ^ b);
+  d = G16_mul(a, b);
+  e = G16_inv(c ^ d);
+  p = G16_mul(e, b);
+  q = G16_mul(e, a);
+  return ((p << 4) | q);
+}
 
 /* convert to new basis in GF(2^8) */
 /* i.e., bit matrix multiply */
-int G256_newbasis( int x, int b[] ) {
-    int i, y = 0;
+int G256_newbasis(int x, int b[]) {
+  int i, y = 0;
 
-    for ( i=7; i >= 0; i-- ) {
-        if ( x & 1 ) y ^= b[i];
-        x >>= 1;
-        }
-    return ( y );
-    }
+  for (i = 7; i >= 0; i--) {
+    if (x & 1)
+      y ^= b[i];
+    x >>= 1;
+  }
+  return (y);
+}
 
 /* find Sbox of n in GF(2^8) mod POLY */
-int Sbox( int n ) {
-     int t;
+int Sbox(int n) {
+  int t;
 
-    t = G256_newbasis( n, A2X );
-    t = G256_inv( t );
-    t = G256_newbasis( t, X2S );
-    return ( t ^ 0x63 );
-    }
+  t = G256_newbasis(n, A2X);
+  t = G256_inv(t);
+  t = G256_newbasis(t, X2S);
+  return (t ^ 0x63);
+}
 
 /* find inverse Sbox of n in GF(2^8) mod POLY */
-int iSbox( int n ) {
-     int t;
+int iSbox(int n) {
+  int t;
 
-    t = G256_newbasis( n ^ 0x63, S2X );
-    t = G256_inv( t );
-    t = G256_newbasis( t, X2A );
-    return ( t );
-    }
+  t = G256_newbasis(n ^ 0x63, S2X);
+  t = G256_inv(t);
+  t = G256_newbasis(t, X2A);
+  return (t);
+}
 
 /* compute tables of Sbox & its inverse; print 'em out */
-int main () {
-     int Sbox_tbl[256], iSbox_tbl[256], i, j;
+int main() {
+  int Sbox_tbl[256], iSbox_tbl[256], i, j;
 
-    for (i = 0; i < 256; i++) {
-        Sbox_tbl[i] = Sbox(i);
-        iSbox_tbl[i] = iSbox(i);
-        }
-    printf ("char S[256] = {\n");
-    for (i = 0; i < 16; i++) {
+  for (i = 0; i < 256; i++) {
+    Sbox_tbl[i] = Sbox(i);
+    iSbox_tbl[i] = iSbox(i);
+  }
+  printf("char S[256] = {\n");
+  for (i = 0; i < 16; i++) {
     for (j = 0; j < 16; j++) {
-        printf ( "%3d, ", Sbox_tbl[i*16+j]);
-        }
-        printf ( "\n" );
-        }
-        printf ( "};\n\n" );
-    printf ("char Si[256] = {\n");
-    for (i = 0; i < 16; i++) {
-    for (j = 0; j < 16; j++) {
-        printf ( "%3d, ", iSbox_tbl[i*16+j]);
-        }
-        printf ( "\n" );
-        }
-        printf ( "};\n\n" );
-    return(0);
+      printf("%3d, ", Sbox_tbl[i * 16 + j]);
     }
+    printf("\n");
+  }
+  printf("};\n\n");
+  printf("char Si[256] = {\n");
+  for (i = 0; i < 16; i++) {
+    for (j = 0; j < 16; j++) {
+      printf("%3d, ", iSbox_tbl[i * 16 + j]);
+    }
+    printf("\n");
+  }
+  printf("};\n\n");
+  return (0);
+}
